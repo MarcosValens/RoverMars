f78f870bfe30a542891f05dc671e32fb1828f94c
marcos <marcosvalens@gmail.com>
2019-10-15 21:05:02 +0200

    Parser photo ok!

--- src/main/java/com/esliceu/service/RoverService.java
+++ src/main/java/com/esliceu/service/RoverService.java
@@ -1,10 +1,12 @@
 package com.esliceu.service;
 
 import com.eslicue.model.CameraAbbr;
+import com.eslicue.model.Photo;
 import com.eslicue.model.RoverName;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 import java.io.BufferedReader;
@@ -12,36 +14,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoverService {
     private static final String API_KEY_NASA = "ShV0umuKcz7vJybpcvHldBMGOLzgb3elRrfbeSJw";
-    public void getByMartianSol(int sol, CameraAbbr cameraAbbr, int page, RoverName roverName) throws IOException {
-        URL client = new URL("https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverName+"/photos?sol="+sol+"&camera="+cameraAbbr+"&api_key="+API_KEY_NASA);
+
+    public List<Photo> getByMartianSol(int sol, CameraAbbr cameraAbbr, int page, RoverName roverName) throws IOException {
+        URL client = new URL("https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverName + "/photos?sol=" + sol + "&camera=" + cameraAbbr + "&api_key=" + API_KEY_NASA);
         URLConnection conexion = client.openConnection();
         conexion.connect();
         BufferedReader in = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
         String inputLine;
         String outputLine = "";
+        List<Photo> photos = new ArrayList<>();
         while ((inputLine = in.readLine()) != null) {
             outputLine += inputLine;
         }
-        System.out.println(outputLine);
         try {
-            JSONParser parser = new JSONParser();
-            JSONObject principal = (JSONObject) parser.parse(outputLine);
-            JSONObject photoData = (JSONObject) principal.get("photos");
+            JSONParser parserNasa = new JSONParser();
+            JSONObject objNasa;
+            objNasa = (JSONObject) parserNasa.parse(outputLine);
+            JSONArray photoObjects = (JSONArray) objNasa.get("photos");
+            System.out.println(objNasa);
+            for (Object object : photoObjects) {
+                JSONObject photoObject = (JSONObject) object;
+                String url = (String) photoObject.get("img_src");
+                String earthDate = (String) photoObject.get("earth_date");
+                String[] descompossedDate = earthDate.split("-");
+                LocalDate date = LocalDate.of(Integer.parseInt(descompossedDate[0]), Integer.parseInt(descompossedDate[1]), Integer.parseInt(descompossedDate[2]));
+                Photo photo = new Photo(date, url);
+                photos.add(photo);
 
-
-
-            /*JSONArray meteoritos = (JSONArray) meteoritosData.get(date);
-            meteoritos.forEach(meteorito -> {
-                System.out.println(meteorito);
-            });*/
-            //recorrer los meteoritos y guardarlos dentro de un array
-
-        } catch (org.json.simple.parser.ParseException e){
-            e.printStackTrace();
+            }
+        } catch (ParseException ignored) {
         }
         in.close();
+        return photos;
     }
 }
--- src/main/java/com/eslicue/model/Photo.java
+++ src/main/java/com/eslicue/model/Photo.java
@@ -4,4 +4,26 @@ import java.time.LocalDate;
 
 public class Photo {
     private LocalDate date;
+    private String url;
+
+    public Photo(LocalDate date, String url) {
+        this.date = date;
+        this.url = url;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
